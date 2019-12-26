24a21c34f359513310b760f95c871a9d1ee01903
Maksim Eltyshev <meltyshev@gmail.com>
2019-12-26 17:24:47 +0500

    Update delete-label.js

--- server/api/helpers/delete-label.js
+++ server/api/helpers/delete-label.js
@@ -11,7 +11,7 @@ module.exports = {
 
   async fn(inputs, exits) {
     await CardLabel.destroy({
-      labelId: inputs.record.id,
+      labelId: input.srecord.id,
     });
 
     const label = await Label.archiveOne(inputs.record.id);
