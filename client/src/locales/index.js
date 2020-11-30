6fc8ec8a2da15aca4bcf5a1e7ec66fb00ed43476
Maksim Eltyshev <meltyshev@gmail.com>
2020-11-30 21:04:03 +0500

    Add german language. Closes #54

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -1,10 +1,12 @@
 import fromPairs from 'lodash/fromPairs';
 
 import en from './en/embed';
+import de from './de/embed';
 import ru from './ru/embed';
 
 const localePairs = [
   ['en', en],
+  ['de', de],
   ['ru', ru],
 ];
 
