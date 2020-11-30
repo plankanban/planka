620b66748beae0a12666f1b3f8f8d7aff9364cd1
timohzbs <43761260+timohzbs@users.noreply.github.com>
2020-11-30 16:43:25 +0100

    Add embed.js de locale

---
+++ client/src/locales/de/embed.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-Mail oder Benutzername',
+      invalidEmailOrUsername: 'Ungültige E-Mail oder Benutzername',
+      invalidPassword: 'Ungültiges Passwort',
+      logInToPlanka: 'Einloggen',
+      noInternetConnection: 'Keine Internetverbindung',
+      pageNotFound_title: 'Seite nicht gefunden',
+      password: 'Passwort',
+      projectManagment: 'Projekt Management',
+      serverConnectionFailed: 'Serververbindung fehlgeschlagen',
+      unknownError: 'Unbekannter Fehler, bitte später erneut versuchen',
+    },
+
+    action: {
+      logIn: 'Einloggen',
+    },
+  },
+};
