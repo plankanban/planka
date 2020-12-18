8964b1f168b7f94bfbec06da28a7ba327854eb54
Piotr <piotr@gorszyportal.pl>
2020-12-18 10:40:03 +0100

    Added translation to Polish language

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -3,11 +3,13 @@ import fromPairs from 'lodash/fromPairs';
 import en from './en/embed';
 import de from './de/embed';
 import ru from './ru/embed';
+import pl from './pl/embed';
 
 const localePairs = [
   ['en', en],
   ['de', de],
   ['ru', ru],
+  ['pl', pl],
 ];
 
 export const languages = localePairs.map((locale) => locale[0]);
