ba25aaf01f1771449e137f25570051b2b5049b2d
pkrolkgp <32438012+pkrolkgp@users.noreply.github.com>
2020-12-20 14:37:55 +0100

    Add polish language

---
+++ client/src/locales/pl/embed.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail lub nazwa użytkownika',
+      invalidEmailOrUsername: 'Błędny e-mail lub nazwa użytkownika',
+      invalidPassword: 'Błędne Hasło',
+      logInToPlanka: 'Zaloguj do Planki',
+      noInternetConnection: 'Brak połączenia z internetem',
+      pageNotFound_title: 'Nie znaleziono strony',
+      password: 'Hasło',
+      projectManagment: 'Zarządzanie projektem',
+      serverConnectionFailed: 'Błąd połączenia z serwerem',
+      unknownError: 'Nieznany błąd, spróbuj ponownie później',
+    },
+
+    action: {
+      logIn: 'Zaloguj',
+    },
+  },
+};
