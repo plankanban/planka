37ede66567cc8c27659fc74aa0cecffa48075679
NickKelly1 <nick.kelly314+github@gmail.com>
2021-02-15 11:26:15 -0800

    enable redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose as reduxCompose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware } from 'connected-react-router';
 
@@ -10,12 +10,19 @@ const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware, routerMiddleware(history)];
 
+let compose = reduxCompose;
+
 if (process.env.NODE_ENV !== 'production') {
   const { logger } = require('redux-logger'); // eslint-disable-line global-require
-
   middlewares.push(logger);
+
+  // enable Redux Devtools in development
+  // https://github.com/zalmoxisus/redux-devtools-extension
+  if (typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined') {
+    compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  }
 }
 
-export default createStore(rootReducer, applyMiddleware(...middlewares));
+export default createStore(rootReducer, compose(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(rootSaga);
