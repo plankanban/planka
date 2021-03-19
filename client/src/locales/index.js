def113456695300d5a5d646d277198d8afaa04b9
nielsboussion <74178491+nielsboussion@users.noreply.github.com>
2021-03-19 22:30:45 +0100

    Add French language

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -4,12 +4,14 @@ import en from './en/embed';
 import de from './de/embed';
 import pl from './pl/embed';
 import ru from './ru/embed';
+import fr from './fr/embed';
 
 const localePairs = [
   ['en', en],
   ['de', de],
   ['ru', ru],
   ['pl', pl],
+  ['fr', fr],
 ];
 
 export const languages = localePairs.map((locale) => locale[0]);
