c081e322bea2f81305833119eebde94c27aaf634
Santiago Forero <thesantiagoforero@gmail.com>
2021-04-26 02:02:22 +0000

    add all spanish translations

---
+++ client/src/locales/es/embed.js
@@ -0,0 +1,21 @@
+export default {
+    translation: {
+      common: {
+        emailOrUsername: 'Correo o nombre de usuario',
+        invalidEmailOrUsername: 'Correo o nombre de usuario incorrecto',
+        invalidPassword: 'Contraseña incorrecta',
+        logInToPlanka: 'Iniciar sesión en Planka',
+        noInternetConnection: 'Sin conexión a internet',
+        pageNotFound_title: 'Página no encontrada',
+        password: 'Contraseña',
+        projectManagment: 'Gestión de Proyectos',
+        serverConnectionFailed: 'Conexión con el servidor fallida',
+        unknownError: 'Error desconocido, intenta más tarde',
+      },
+  
+      action: {
+        logIn: 'Iniciar sesión',
+      },
+    },
+  };
+  
