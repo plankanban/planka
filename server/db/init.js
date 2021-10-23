05dbafd1d128f94f28e62b68aef846b2dfe0c105
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2021-10-23 20:37:27 +0100

    Migrations: alway apply migrations on start-up

--- server/db/init.js
+++ server/db/init.js
@@ -6,8 +6,8 @@ const knex = require('knex')(config); // eslint-disable-line import/order
   try {
     const isExists = await knex.schema.hasTable(config.migrations.tableName);
 
+    await knex.migrate.latest();
     if (!isExists) {
-      await knex.migrate.latest();
       await knex.seed.run();
     }
   } catch (error) {
