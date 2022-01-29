9cdea39c2bef8c93e3ecac150d3748c74a76d270
Christiansen <14914468+martinmch@users.noreply.github.com>
2022-01-29 22:46:41 +0100

    Add Danish translation

---
+++ client/src/locales/da/embed.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail eller brugernavn',
+      invalidEmailOrUsername: 'Ugyldig e-mail eller brugernavn',
+      invalidPassword: 'Ugyldig løsen',
+      logInToPlanka: 'Log på Planka',
+      noInternetConnection: 'Ingen forbindelse til internettet',
+      pageNotFound_title: 'Side ej fundet',
+      password: 'Løsen',
+      projectManagement: 'Projektstyring',
+      serverConnectionFailed: 'Ingen forbindelse til serveren',
+      unknownError: 'Ukendt fejl - prøv igen',
+    },
+
+    action: {
+      logIn: 'Log på',
+    },
+  },
+};
