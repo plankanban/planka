68f897c1b8534d8db97d7da02b26b9a32b6b7ba9
LuisLiu <lc63msn@hotmail.com>
2022-03-08 20:24:49 +0800

    [language]：add chinese local file

---
+++ client/src/locales/zh/embed.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: '邮箱或用户名',
+      invalidEmailOrUsername: '错误的邮箱或用户名',
+      invalidPassword: '密码错误',
+      logInToPlanka: '登录至 Planka',
+      noInternetConnection: '没有网络连接',
+      pageNotFound_title: '页面找不到',
+      password: '密码',
+      projectManagement: '项目管理',
+      serverConnectionFailed: '服务连接失败',
+      unknownError: '未知错误，请稍后重试',
+    },
+
+    action: {
+      logIn: '登录',
+    },
+  },
+};
