d5a6f57fa788c47498bdccb8bfb60ff1fb82286d
Maksim Eltyshev <meltyshev@gmail.com>
2022-04-21 04:20:04 +0500

    ref: Fix locale file names

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -1,29 +1,29 @@
 import fromPairs from 'lodash/fromPairs';
 
-import cs from './cs/embed';
-import da from './da/embed';
-import de from './de/embed';
-import en from './en/embed';
-import es from './es/embed';
-import fr from './fr/embed';
-import ja from './ja/embed';
-import pl from './pl/embed';
-import ru from './ru/embed';
-import uz from './uz/embed';
-import zh from './zh/embed';
+import csLogin from './cs/login';
+import daLogin from './da/login';
+import deLogin from './de/login';
+import enLogin from './en/login';
+import esLogin from './es/login';
+import frLogin from './fr/login';
+import jaLogin from './ja/login';
+import plLogin from './pl/login';
+import ruLogin from './ru/login';
+import uzLogin from './uz/login';
+import zhLogin from './zh/login';
 
 const localePairs = [
-  ['cs', cs],
-  ['da', da],
-  ['de', de],
-  ['en', en],
-  ['es', es],
-  ['fr', fr],
-  ['ja', ja],
-  ['pl', pl],
-  ['ru', ru],
-  ['uz', uz],
-  ['zh', zh],
+  ['cs', csLogin],
+  ['da', daLogin],
+  ['de', deLogin],
+  ['en', enLogin],
+  ['es', esLogin],
+  ['fr', frLogin],
+  ['ja', jaLogin],
+  ['pl', plLogin],
+  ['ru', ruLogin],
+  ['uz', uzLogin],
+  ['zh', zhLogin],
 ];
 
 export const languages = localePairs.map((locale) => locale[0]);
