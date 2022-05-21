81052e1ce6a1ba0bead5ddc1962b9ca8dfbd99d3
André Wahlberg <97anwa50@gmail.com>
2022-05-21 17:39:17 +0200

    feat: Add Swedish translation (#246)

---
+++ client/src/locales/se/login.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail eller användarnamn',
+      invalidEmailOrUsername: 'Ogiltig e-mail eller användarnamn',
+      invalidPassword: 'Ogiltigt lösenord',
+      logInToPlanka: 'Logga in på Planka',
+      noInternetConnection: 'Ingen internetanslutning',
+      pageNotFound_title: 'Sidan Kunde Inte Hittas',
+      password: 'Lösenord',
+      projectManagement: 'Projektledning',
+      serverConnectionFailed: 'Server connection failed',
+      unknownError: 'Okänt fel, försök igen senare',
+    },
+
+    action: {
+      logIn: 'Logga in',
+    },
+  },
+};
