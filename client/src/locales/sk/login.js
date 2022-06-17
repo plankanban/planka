ba850801c584030efc8f08190e6e6828d4228f11
Dominik Dancs <dominik@dancs.sk>
2022-06-17 17:16:15 +0200

    feat: Add Slovak translation (#265)

---
+++ client/src/locales/sk/login.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail alebo používateľské meno',
+      invalidEmailOrUsername: 'Nesprávny e-mail alebo používateľské meno',
+      invalidPassword: 'Nesprávne heslo',
+      logInToPlanka: 'Prihlásiť sa do Planka',
+      noInternetConnection: 'Bez pripojenia k internetu',
+      pageNotFound_title: 'Stránka neexistuje',
+      password: 'Heslo',
+      projectManagement: 'Správa projektu',
+      serverConnectionFailed: 'Pripojenie k serveru zlyhalo',
+      unknownError: 'Neznáma chyba, skúste to neskôr',
+    },
+
+    action: {
+      logIn: 'Prihlásiť sa',
+    },
+  },
+};
