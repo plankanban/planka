e9a65bb6419efc90b7be493c975858120b721779
Simon Tagne <40598597+SimonTagne@users.noreply.github.com>
2022-08-03 18:34:16 +0200

    fix: Add Secure and SameSite to authentication token
    Cf. #275

--- client/src/utils/access-token-storage.js
+++ client/src/utils/access-token-storage.js
@@ -5,10 +5,25 @@ import Config from '../constants/Config';
 export const setAccessToken = (accessToken) => {
   Cookies.set(Config.ACCESS_TOKEN_KEY, accessToken, {
     expires: Config.ACCESS_TOKEN_EXPIRES,
+    secure: window.location.protocol === 'https:',
+    sameSite: 'strict',
+  });
+  Cookies.set(Config.ACCESS_TOKEN_VERSION_KEY, Config.ACCESS_TOKEN_VERSION, {
+    expires: Config.ACCESS_TOKEN_EXPIRES,
   });
 };
 
-export const getAccessToken = () => Cookies.get(Config.ACCESS_TOKEN_KEY);
+export const getAccessToken = () => {
+  // TODO: remove migration
+  const accessToken = Cookies.get(Config.ACCESS_TOKEN_KEY);
+  const accessTokenVersion = Cookies.get(Config.ACCESS_TOKEN_VERSION_KEY);
+  if (accessToken && accessTokenVersion !== Config.ACCESS_TOKEN_VERSION) {
+    // Add secure and sameSite attributes to the cookie
+    setAccessToken(accessToken);
+  }
+
+  return accessToken;
+};
 
 export const removeAccessToken = () => {
   Cookies.remove(Config.ACCESS_TOKEN_KEY);
