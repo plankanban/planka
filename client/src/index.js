8069545fa90e4f8b5b55ac6f0bc523c830b158c4
Maksim Eltyshev <meltyshev@gmail.com>
2022-11-21 01:12:32 +0100

    ref: Little refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
 import store, { history } from './store';
 import Root from './components/Root';
 
 import './i18n';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(Root, { store, history }));
