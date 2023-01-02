62adac2f58ac52b97a043e1e62920fa884ed7f33
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2023-01-02 20:05:35 +0100

    Update login.js

--- client/src/locales/it/login.js
+++ client/src/locales/it/login.js
@@ -2,13 +2,13 @@ export default {
   translation: {
     common: {
       emailOrUsername: 'E-mail o username',
-      invalidEmailOrUsername: 'E-mail or username invalido',
+      invalidEmailOrUsername: 'E-mail o username non valido',
       invalidPassword: 'Password non valida',
       logInToPlanka: 'Log in Planka',
       noInternetConnection: 'Nessuna connessione internet',
       pageNotFound_title: 'Pagina non trovata',
       password: 'Password',
-      projectManagement: 'Project management',
+      projectManagement: 'Gestione del progetto',
       serverConnectionFailed: 'Connesione al server fallita',
       unknownError: 'Errore sconosciuto, prova ancora',
     },
