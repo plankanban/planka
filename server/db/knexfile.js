d627a5660a1504b9fc3e169c8db2d31b6cc4c4a3
orbatschow <nils@orbat.sh>
2023-02-27 17:16:51 +0100

    feat: Allow postgres connections that require ssl mode (#404)
    Closes #261

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -8,7 +8,12 @@ dotenv.config({
 
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   migrations: {
     tableName: 'migration',
     directory: path.join(__dirname, 'migrations'),
