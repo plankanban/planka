6aeaa544a6314854e65e8767ffa36faaed5014ad
Maksim Eltyshev <meltyshev@gmail.com>
2023-03-06 13:03:19 +0100

    ref: Add file to resolve conflict

---
+++ client/src/locales/index.js
@@ -0,0 +1,29 @@
+import cs from './cs';
+import da from './da';
+import de from './de';
+import en from './en';
+import es from './es';
+import fr from './fr';
+import it from './it';
+import ja from './ja';
+import ko from './ko';
+import pl from './pl';
+import ru from './ru';
+import sk from './sk';
+import sv from './sv';
+import uz from './uz';
+import zh from './zh';
+
+const locales = [cs, da, de, en, es, fr, it, ja, ko, pl, ru, sk, sv, uz, zh];
+
+export default locales;
+
+export const languages = locales.map((locale) => locale.language);
+
+export const embeddedLocales = locales.reduce(
+  (result, locale) => ({
+    ...result,
+    [locale.language]: locale.embeddedLocale,
+  }),
+  {},
+);
