af9bdda1920e1e7dd143594284ea32afbbaf662e
Jeffrey <jeff@jjakt.monster>
2023-09-01 05:21:28 -0500

    used base url

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Config from './constants/Config';
 import store from './store';
 import history from './history';
 import Root from './components/Root';
 import './i18n';
 
-fetch('http://localhost:1337/api/appconfig').then((response) => {
+fetch(`${Config.SERVER_BASE_URL}/api/appconfig`).then((response) => {
   response.json().then((config) => {
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(React.createElement(Root, { store, history, config }));
