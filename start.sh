2f50b0e8cf551cdffd3d583c0626db0b1839a7f1
NavyStack <137406386+Navystack@users.noreply.github.com>
2023-12-18 14:17:09 +0900

    Hide docker-compose command to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-node db/init.js
-exec node app.js --prod $@
+
+for i in $(seq 1 30); do
+  echo "Attempting to initialize the database and start the Planka (attempt $i)..."
+  node db/init.js &&
+  exec node app.js --prod "$@" &&
+  break || s=$?;
+  echo "Failed (attempt $i). Waiting 5 seconds before the next attempt...";
+  sleep 5;
+done
+
+exit $s
