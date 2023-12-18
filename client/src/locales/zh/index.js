49219e50512ff53a925e510cfe8e4c2dfe8fc805
ossdate <info@ossdate.com>
2023-12-18 20:25:48 +0800

    fix zh translation of 1.15.2 version

--- client/src/locales/zh/index.js
+++ client/src/locales/zh/index.js
@@ -1,8 +1,11 @@
+import merge from 'lodash/merge';
+
 import login from './login';
+import core from './core';
 
 export default {
   language: 'zh',
   country: 'cn',
   name: '中文',
-  embeddedLocale: login,
+  embeddedLocale: merge(login, core),
 };
