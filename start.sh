7c55e25d97ade7ba92eec0ca7495ca57c837d1ad
NavyStack <137406386+Navystack@users.noreply.github.com>
2023-12-19 09:08:43 +0900

    .

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -e
-
-for i in $(seq 1 30); do
-  echo "Attempting to initialize the database and start the Planka (attempt $i)..."
-  node db/init.js &&
-  exec node app.js --prod "$@" &&
-  break || s=$?;
-  echo "Failed (attempt $i). Waiting 5 seconds before the next attempt...";
-  sleep 5;
-done
-
-exit $s
+node db/init.js
+exec node app.js --prod $@
