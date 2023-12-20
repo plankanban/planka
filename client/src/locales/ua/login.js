2f3a0c34c636c65a5efa41f01bfb972c135363e0
Denis Polishchuk <52839937+denipolis@users.noreply.github.com>
2023-12-20 13:39:51 +0200

    feat: Add Ukrainian translation (#571)

---
+++ client/src/locales/ua/login.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: "Електронна пошта або ім'я користувача",
+      invalidEmailOrUsername: "Неправильна електронна пошта або ім'я користувача",
+      invalidPassword: 'Неправильний пароль',
+      logInToPlanka: 'Увійти в Planka',
+      noInternetConnection: 'Відсутнє підключення до Інтернету',
+      pageNotFound_title: 'Сторінку не знайдено',
+      password: 'Пароль',
+      projectManagement: 'Управління проектами',
+      serverConnectionFailed: 'Не вдалося підключитися до сервера',
+      unknownError: 'Невідома помилка, спробуйте ще раз пізніше',
+      useSingleSignOn: 'Використовувати одночасний вхід',
+    },
+
+    action: {
+      logIn: 'Увійти',
+      logInWithSSO: 'Увійти за допомогою SSO',
+    },
+  },
+};
