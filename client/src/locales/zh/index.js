c3e6a348efb558bc37f667d9de6019c0a6d1c2ac
Maksim Eltyshev <meltyshev@gmail.com>
2023-12-20 12:32:04 +0100

    fix: Fix merging

--- client/src/locales/zh/index.js
+++ client/src/locales/zh/index.js
@@ -1,11 +1,8 @@
-import merge from 'lodash/merge';
-
 import login from './login';
-import core from './core';
 
 export default {
   language: 'zh',
   country: 'cn',
   name: '中文',
-  embeddedLocale: merge(login, core),
+  embeddedLocale: login,
 };
