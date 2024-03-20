0d3cf0a32d5f78a38bb9a742778858bc0966aff0
Chris <54485661+trismu7@users.noreply.github.com>
2024-03-20 10:55:17 -0300

    feat: Add Portuguese translation (#640)

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -8,6 +8,7 @@ import it from './it';
 import ja from './ja';
 import ko from './ko';
 import pl from './pl';
+import pt from './pt';
 import ro from './ro';
 import ru from './ru';
 import sk from './sk';
@@ -17,7 +18,7 @@ import ua from './ua';
 import uz from './uz';
 import zh from './zh';
 
-const locales = [cs, da, de, en, es, fr, it, ja, ko, pl, ro, ru, sk, sv, tr, ua, uz, zh];
+const locales = [cs, da, de, en, es, fr, it, ja, ko, pl, pt, ro, ru, sk, sv, tr, ua, uz, zh];
 
 export default locales;
 
