7acaec6d04d347509544c6247697b26f8d197200
Maksim Eltyshev <meltyshev@gmail.com>
2024-07-15 14:01:06 +0200

    fix: Fix incorrect related data when transferring card
    Closes #431, closes #803

--- client/src/models/Attachment.js
+++ client/src/models/Attachment.js
@@ -28,6 +28,7 @@ export default class extends BaseModel {
       case ActionTypes.CORE_INITIALIZE:
       case ActionTypes.PROJECT_MANAGER_CREATE_HANDLE:
       case ActionTypes.BOARD_MEMBERSHIP_CREATE_HANDLE:
+      case ActionTypes.CARD_UPDATE_HANDLE:
         if (payload.attachments) {
           payload.attachments.forEach((attachment) => {
             Attachment.upsert(attachment);
