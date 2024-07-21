76c78c72527210ccd4ff3a9a70024bc556634af2
Mazyar Yousefiniyae shad <mazyaryousefinia@gmail.com>
2024-07-21 19:51:18 +0330

    feat: Add Persian translation (#828)

--- client/src/locales/index.js
+++ client/src/locales/index.js
@@ -4,6 +4,7 @@ import da from './da';
 import de from './de';
 import en from './en';
 import es from './es';
+import fa from './fa';
 import fr from './fr';
 import hu from './hu';
 import id from './id';
@@ -29,6 +30,7 @@ const locales = [
   de,
   en,
   es,
+  fa,
   fr,
   hu,
   id,
