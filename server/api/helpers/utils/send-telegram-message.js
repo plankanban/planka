cc95032e74e4d83d8e0e6ee8292e0f0e1724e287
Elllone <64659626+Elllone@users.noreply.github.com>
2024-10-31 00:11:52 +0300

    feat: Telegram bot notifications (#928)

---
+++ server/api/helpers/utils/send-telegram-message.js
@@ -0,0 +1,43 @@
+const POST_MESSAGE_API_URL = (telegramBotToken) =>
+  `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+
+module.exports = {
+  inputs: {
+    html: {
+      type: 'string',
+      required: true,
+    },
+  },
+  async fn(inputs) {
+    const headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+    };
+
+    const body = {
+      chat_id: sails.config.custom.telegramChatId,
+      text: inputs.html,
+      parse_mode: 'HTML',
+    };
+
+    if (sails.config.custom.telegramThreadId) {
+      body.message_thread_id = sails.config.custom.telegramThreadId;
+    }
+
+    let response;
+    try {
+      response = await fetch(POST_MESSAGE_API_URL(sails.config.custom.telegramBotToken), {
+        headers,
+        method: 'POST',
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      sails.log.error(`Error sending to Telegram: ${error}`);
+      return;
+    }
+
+    if (!response.ok) {
+      const responseErrorJson = await response.json();
+      sails.log.error(`Error sending to Telegram: ${responseErrorJson.description}`);
+    }
+  },
+};
