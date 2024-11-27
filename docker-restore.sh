a3f101a1396564465a04a3118274939473c36163
Rafał Florczak <florczakraf@users.noreply.github.com>
2024-11-27 11:49:55 +0100

    fix: Quote variables to prevent word splitting (#962)

--- docker-restore.sh
+++ docker-restore.sh
@@ -9,29 +9,29 @@ PLANKA_DOCKER_CONTAINER_PLANKA="planka_planka_1"
 
 # Extract tgz archive
 PLANKA_BACKUP_ARCHIVE_TGZ=$1
-PLANKA_BACKUP_ARCHIVE=$(basename $PLANKA_BACKUP_ARCHIVE_TGZ .tgz)
+PLANKA_BACKUP_ARCHIVE=$(basename "$PLANKA_BACKUP_ARCHIVE_TGZ" .tgz)
 echo -n "Extracting tarball $PLANKA_BACKUP_ARCHIVE_TGZ ... "
-tar -xzf $PLANKA_BACKUP_ARCHIVE_TGZ
+tar -xzf "$PLANKA_BACKUP_ARCHIVE_TGZ"
 echo "Success!"
 
 # Import Database
 echo -n "Importing postgres database ... "
-cat $PLANKA_BACKUP_ARCHIVE/postgres.sql | docker exec -i $PLANKA_DOCKER_CONTAINER_POSTGRES psql -U postgres
+cat "$PLANKA_BACKUP_ARCHIVE/postgres.sql" | docker exec -i "$PLANKA_DOCKER_CONTAINER_POSTGRES" psql -U postgres
 echo "Success!"
 
 # Restore Docker Volumes
 echo -n "Importing user-avatars ... "
-docker run --rm --volumes-from $PLANKA_DOCKER_CONTAINER_PLANKA -v $(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup ubuntu cp -rf /backup/user-avatars /app/public/
+docker run --rm --volumes-from "$PLANKA_DOCKER_CONTAINER_PLANKA" -v "$(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup" ubuntu cp -rf /backup/user-avatars /app/public/
 echo "Success!"
 echo -n "Importing project-background-images ... "
-docker run --rm --volumes-from $PLANKA_DOCKER_CONTAINER_PLANKA -v $(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup ubuntu cp -rf /backup/project-background-images /app/public/
+docker run --rm --volumes-from "$PLANKA_DOCKER_CONTAINER_PLANKA" -v "$(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup" ubuntu cp -rf /backup/project-background-images /app/public/
 echo "Success!"
 echo -n "Importing attachments ... "
-docker run --rm --volumes-from $PLANKA_DOCKER_CONTAINER_PLANKA -v $(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup ubuntu cp -rf /backup/attachments /app/private/
+docker run --rm --volumes-from "$PLANKA_DOCKER_CONTAINER_PLANKA" -v "$(pwd)/$PLANKA_BACKUP_ARCHIVE:/backup" ubuntu cp -rf /backup/attachments /app/private/
 echo "Success!"
 
 echo -n "Cleaning up temporary files and folders ... "
-rm -r $PLANKA_BACKUP_ARCHIVE
+rm -r "$PLANKA_BACKUP_ARCHIVE"
 echo "Success!"
 
 echo "Restore complete!"
