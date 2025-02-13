22270d4c774bb8762cbc55cc65393026fe746d50
Marc-Andrieu <146140470+Marc-Andrieu@users.noreply.github.com>
2025-02-13 12:51:26 +0100

    Fix: fr-FR translations

--- client/src/locales/fr-FR/login.js
+++ client/src/locales/fr-FR/login.js
@@ -1,16 +1,16 @@
 export default {
   translation: {
     common: {
-      emailOrUsername: "Email ou nom d'utilisateur",
-      invalidEmailOrUsername: "Email ou nom d'utilisateur invalide",
+      emailOrUsername: "E-mail ou nom d'utilisateur",
+      invalidEmailOrUsername: "E-mail ou nom d'utilisateur invalide",
       invalidCredentials: 'Identifiants invalides',
       invalidPassword: 'Mot de passe invalide',
       logInToPlanka: 'Se connecter à Planka',
-      noInternetConnection: 'Aucune connection internet',
+      noInternetConnection: 'Aucune connexion Internet',
       pageNotFound_title: 'Page non trouvée',
       password: 'Mot de passe',
       projectManagement: 'Gestion de projet',
-      serverConnectionFailed: 'Connection au serveur échouée',
+      serverConnectionFailed: 'Échec de la connexion au serveur',
       unknownError: 'Erreur inconnue, réessayez plus tard',
       useSingleSignOn: "Utiliser l'authentification unique",
     },
