aae4251ca4ce3a3030a52dbbbc1701da994d3550
NavyStack <navystack@askfront.com>
2025-03-17 05:21:40 +0900

    fix: Update Korean translation (#1060)

--- client/src/locales/ko-KR/login.js
+++ client/src/locales/ko-KR/login.js
@@ -1,22 +1,23 @@
 export default {
   translation: {
     common: {
-      emailOrUsername: '이메일 혹은 사용자 이름',
-      invalidEmailOrUsername: '이메일 혹은 사용자 이름이 유효하지 않습니다',
-      invalidPassword: '유효하지 않은 비밀번호',
+      emailOrUsername: '이메일 또는 사용자 이름',
+      invalidEmailOrUsername: '잘못된 이메일 또는 사용자 이름',
+      invalidCredentials: '잘못된 자격 증명',
+      invalidPassword: '잘못된 비밀번호',
       logInToPlanka: 'Planka에 로그인',
-      noInternetConnection: '인터넷 연결이 없음',
-      pageNotFound_title: '페이지를 찾을 수 없습니다',
+      noInternetConnection: '인터넷 연결 없음',
+      pageNotFound_title: '페이지를 찾을 수 없음',
       password: '비밀번호',
       projectManagement: '프로젝트 관리',
-      serverConnectionFailed: '서버 연결에 실패함',
-      unknownError: '알 수 없는 오류, 나중에 다시 시도하십시오',
-      useSingleSignOn: 'single sign-on(SSO) 사용',
+      serverConnectionFailed: '서버 연결 실패',
+      unknownError: '알 수 없는 오류, 나중에 다시 시도하세요',
+      useSingleSignOn: 'Single Sign-On(SSO) 사용',
     },
 
     action: {
       logIn: '로그인',
-      logInWithSSO: 'single sign-on(SSO)으로 로그인',
+      logInWithSSO: 'SSO로 로그인',
     },
   },
 };
