c1f6a5c285c403898d808aece511004dbb91addb
Pedro <npedrini2003@icloud.com>
2025-05-10 16:58:13 +0200

    updated italian locales to 2.0

--- client/src/locales/it-IT/login.js
+++ client/src/locales/it-IT/login.js
@@ -1,20 +1,26 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'Limite utenti attivi raggiunto',
       emailAlreadyInUse: 'E-mail già in uso',
       emailOrUsername: 'E-mail o username',
+      invalidCredentials: 'Credenziali non valide',
       invalidEmailOrUsername: 'E-mail o username non valido',
       invalidPassword: 'Password non valida',
+      logIn_title: 'Accedi',
       noInternetConnection: 'Nessuna connessione internet',
+      or: 'Oppure',
       pageNotFound_title: 'Pagina non trovata',
       password: 'Password',
+      poweredByPlanka: 'Powered by Planka',
       serverConnectionFailed: 'Connesione al server fallita',
       unknownError: 'Errore sconosciuto, prova ancora',
       usernameAlreadyInUse: 'Username già in uso',
+      useSingleSignOn: 'Accedi con SSO',
     },
 
     action: {
-      logIn: 'Log in',
+      logIn: 'Accedi',
     },
   },
 };
