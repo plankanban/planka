307f4b1d01de89ca82d3109a29f8d383ebb3cefc
leroyloren <57643470+leroyloren@users.noreply.github.com>
2025-05-12 11:35:52 +0200

    fix: Update Czech translation (#1120)

--- client/src/locales/cs-CZ/login.js
+++ client/src/locales/cs-CZ/login.js
@@ -1,14 +1,18 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'Dosažený limit aktivních uživatelů',
       emailAlreadyInUse: 'E-mail se již používá',
       emailOrUsername: 'E-mail nebo uživatelské jméno',
       invalidCredentials: 'Neplatné přihlašovací údaje',
       invalidEmailOrUsername: 'Nesprávný e-mail nebo uživatelské jméno',
       invalidPassword: 'Nesprávné heslo',
+      logIn_title: 'Přihlásit',
       noInternetConnection: 'Bez připojení k internetu',
+      or: 'Nebo',
       pageNotFound_title: 'Stránka nenalezena',
       password: 'Heslo',
+      poweredByPlanka: 'Poháněno technologií PLANKA',
       serverConnectionFailed: 'Připojení k serveru selhalo',
       unknownError: 'Neznámá chyba, zkuste to později',
       usernameAlreadyInUse: 'Uživatelské jméno se již používá',
