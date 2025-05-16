500a78de0395947ac488b023b60c0db06c23f9d2
HannesOberreiter <hoberreiter@gmail.com>
2025-05-16 09:29:45 +0200

    fix: :bug: add missing login keys

--- client/src/locales/de-DE/login.js
+++ client/src/locales/de-DE/login.js
@@ -1,21 +1,27 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'Maximale Anzahl aktiver Benutzer erreicht',
       emailAlreadyInUse: 'E-mail Adresse wird bereits benutzt',
       emailOrUsername: 'E-Mail-Adresse oder Benutzername',
+      invalidCredentials: 'Ungültige Anmeldeinformationen',
       invalidEmailOrUsername: 'Ungültige E-Mail-Adresse oder Benutzername',
       invalidPassword: 'Ungültiges Passwort',
       logIn_title: 'Anmelden',
       noInternetConnection: 'Keine Internetverbindung',
+      or: 'Oder',
       pageNotFound_title: 'Seite nicht gefunden',
       password: 'Passwort',
+      poweredByPlanka: 'Powered by PLANKA',
       serverConnectionFailed: 'Serververbindung fehlgeschlagen',
       unknownError: 'Unbekannter Fehler, bitte später erneut versuchen',
       usernameAlreadyInUse: 'Benutzername wird bereits verwendet',
+      useSingleSignOn: 'Einmalige Anmeldung (SSO) verwenden',
     },
 
     action: {
       logIn: 'Einloggen',
+      logInWithSso: 'Einloggen mit SSO',
     },
   },
 };
