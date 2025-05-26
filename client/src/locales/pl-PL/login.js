20b5351dfe7aec874642aad7e7a3d2dcb7db9469
Piotr Patalong <piotrpatalong@gmail.com>
2025-05-26 15:02:51 +0200

    fix: Update Polish translation (#1150)

--- client/src/locales/pl-PL/login.js
+++ client/src/locales/pl-PL/login.js
@@ -1,20 +1,27 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'Osiągnięto limit aktywnych użytkowników',
       emailAlreadyInUse: 'E-mail jest już używany',
       emailOrUsername: 'E-mail lub nazwa użytkownika',
+      invalidCredentials: 'Błędne dane logowania',
       invalidEmailOrUsername: 'Błędny e-mail lub nazwa użytkownika',
-      invalidPassword: 'Błędne Hasło',
+      invalidPassword: 'Błędne hasło',
+      logIn_title: 'Logowanie',
       noInternetConnection: 'Brak połączenia z internetem',
-      pageNotFound_title: 'Nie znaleziono strony',
+      or: 'Lub',
+      pageNotFound_title: 'Strona nie znaleziona',
       password: 'Hasło',
+      poweredByPlanka: 'Powered by PLANKA',
       serverConnectionFailed: 'Błąd połączenia z serwerem',
       unknownError: 'Nieznany błąd, spróbuj ponownie później',
-      usernameAlreadyInUse: 'Nazwa użytkownika jest już używana',
+      usernameAlreadyInUse: 'Nazwa użytkownika nie jest dostępna',
+      useSingleSignOn: 'Użyj logowania SSO',
     },
 
     action: {
       logIn: 'Zaloguj',
+      logInWithSso: 'Zaloguj z SSO',
     },
   },
 };
