608a7c983f0a13fb48a652c34299774f7413069e
Christian T R <krede92@gmail.com>
2025-06-05 14:06:45 +0200

    fix: Update Danish translation (#1173)

--- client/src/locales/da-DK/login.js
+++ client/src/locales/da-DK/login.js
@@ -1,20 +1,27 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'Grænsen for aktive brugere er nået',
       emailAlreadyInUse: 'E-mail allerede i brug',
       emailOrUsername: 'E-mail eller brugernavn',
+      invalidCredentials: 'Forkerte loginoplysninger',
       invalidEmailOrUsername: 'Ugyldig e-mail eller brugernavn',
       invalidPassword: 'Ugyldig adgangskode',
+      logIn_title: 'Log på',
       noInternetConnection: 'Ingen forbindelse til internettet',
+      or: 'Eller',
       pageNotFound_title: 'Side ikke fundet',
       password: 'Adgangskode',
+      poweredByPlanka: 'Drevet af <1>PLANKA</1>',
       serverConnectionFailed: 'Ingen forbindelse til serveren',
       unknownError: 'Ukendt fejl - prøv igen',
       usernameAlreadyInUse: 'Brugernavn allerede i brug',
+      useSingleSignOn: 'Anvend single sign-on',
     },
 
     action: {
       logIn: 'Log på',
+      logInWithSso: 'Log på med SSO',
     },
   },
 };
