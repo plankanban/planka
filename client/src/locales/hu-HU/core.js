9ca49f1f51287b8fbdac40b7614e2f4dfc5c0ca7
Symon Baikov <peterbaikov12@gmail.com>
2025-06-08 14:09:31 +0300

    fix: Update Hungarian translation (#1193)

--- client/src/locales/hu-HU/core.js
+++ client/src/locales/hu-HU/core.js
@@ -1,9 +1,12 @@
 import dateFns from 'date-fns/locale/hu';
 import timeAgo from 'javascript-time-ago/locale/hu';
 
+import markdownEditor from './markdown-editor.json';
+
 export default {
   dateFns,
   timeAgo,
+  markdownEditor,
 
   format: {
     date: 'yyyy.M.d.',
