8447f0c88486ca1373b370ddefbfe241b69909a6
Symon Baikov <peterbaikov12@gmail.com>
2025-06-08 14:08:17 +0300

    fix: Update French translation (#1192)

--- client/src/locales/fr-FR/core.js
+++ client/src/locales/fr-FR/core.js
@@ -1,9 +1,12 @@
 import dateFns from 'date-fns/locale/fr';
 import timeAgo from 'javascript-time-ago/locale/fr';
 
+import markdownEditor from './markdown-editor.json';
+
 export default {
   dateFns,
   timeAgo,
+  markdownEditor,
 
   format: {
     date: 'P',
