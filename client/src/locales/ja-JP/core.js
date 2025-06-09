f72ac437b952a55fb947b076008758a1a942ee17
Symon Baikov <peterbaikov12@gmail.com>
2025-06-09 13:32:17 +0300

    fix: Update Japanese translation (#1196)

--- client/src/locales/ja-JP/core.js
+++ client/src/locales/ja-JP/core.js
@@ -1,9 +1,12 @@
 import dateFns from 'date-fns/locale/ja';
 import timeAgo from 'javascript-time-ago/locale/ja';
 
+import markdownEditor from './markdown-editor.json';
+
 export default {
   dateFns,
   timeAgo,
+  markdownEditor,
 
   format: {
     date: 'yyyy/M/d',
