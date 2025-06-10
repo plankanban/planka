e24bb845c62b5448b494c1959fbf24862dc6efd2
Symon Baikov <peterbaikov12@gmail.com>
2025-06-10 13:18:24 +0300

    fix: Update Slovak translation (#1205)

--- client/src/locales/sk-SK/core.js
+++ client/src/locales/sk-SK/core.js
@@ -1,9 +1,12 @@
 import dateFns from 'date-fns/locale/sk';
 import timeAgo from 'javascript-time-ago/locale/sk';
 
+import markdownEditor from './markdown-editor.json';
+
 export default {
   dateFns,
   timeAgo,
+  markdownEditor,
 
   format: {
     date: 'd.M.yyyy',
