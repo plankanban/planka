a55fc08c436a352ec71f0180f4704cf56e2843c5
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-07-07 12:00:36 +0200

    fix: Update French translation (#1238)

--- client/src/locales/fr-FR/login.js
+++ client/src/locales/fr-FR/login.js
@@ -1,14 +1,18 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: 'La limite d’utilisateurs actifs a été atteinte',
       emailAlreadyInUse: 'E-mail déjà utilisé',
       emailOrUsername: "E-mail ou nom d'utilisateur",
       invalidCredentials: 'Identifiants invalides',
       invalidEmailOrUsername: "E-mail ou nom d'utilisateur invalide",
       invalidPassword: 'Mot de passe invalide',
+      logIn_title: 'Se connecter',
       noInternetConnection: 'Aucune connexion Internet',
+      or: 'Ou',
       pageNotFound_title: 'Page non trouvée',
       password: 'Mot de passe',
+      poweredByPlanka: 'Propulsé par <1>PLANKA</1>',
       serverConnectionFailed: 'Échec de la connexion au serveur',
       unknownError: 'Erreur inconnue, réessayez plus tard',
       useSingleSignOn: "Utiliser l'authentification unique",
