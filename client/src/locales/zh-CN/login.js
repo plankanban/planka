49203e9d56e9e71c620cbfbeb1873a215391ae5f
HanJiang-cn <56996238+HanJiang-cn@users.noreply.github.com>
2025-07-09 23:53:58 +0800

    fix: Update Chinese translation (#1250)

--- client/src/locales/zh-CN/login.js
+++ client/src/locales/zh-CN/login.js
@@ -1,13 +1,18 @@
 export default {
   translation: {
     common: {
+      activeUsersLimitReached: '活跃用户数已达上限',
       emailAlreadyInUse: '邮箱已使用',
       emailOrUsername: '邮箱或用户名',
+      invalidCredentials: '无效凭证',
       invalidEmailOrUsername: '无效的邮箱或用户名',
       invalidPassword: '密码错误',
+      logIn_title: '登录',
       noInternetConnection: '没有网络连接',
+      or: '或',
       pageNotFound_title: '找不到页面',
       password: '密码',
+      poweredByPlanka: '技术支持由<1>PLANKA</1>提供',
       serverConnectionFailed: '服务器连接失败',
       unknownError: '未知错误，请稍后重试',
       useSingleSignOn: '使用单点登录',
