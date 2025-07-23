5beafca07e677e94107e744e60221a892024f609
Maksim Eltyshev <meltyshev@gmail.com>
2025-07-23 17:59:57 +0200

    fix: Improve current board context indication

--- client/src/components/boards/BoardActions/BoardActions.jsx
+++ client/src/components/boards/BoardActions/BoardActions.jsx
@@ -6,8 +6,12 @@
 import React from 'react';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { Icon } from 'semantic-ui-react';
 
 import selectors from '../../../selectors';
+import { BoardContexts } from '../../../constants/Enums';
+import { BoardContextIcons } from '../../../constants/Icons';
 import Filters from './Filters';
 import RightSide from './RightSide';
 import BoardMemberships from '../../board-memberships/BoardMemberships';
@@ -15,7 +19,15 @@ import BoardMemberships from '../../board-memberships/BoardMemberships';
 import styles from './BoardActions.module.scss';
 
 const BoardActions = React.memo(() => {
+  const boardContext = useSelector((state) => selectors.selectCurrentBoard(state).context);
+
+  const withContextTitle = boardContext !== BoardContexts.BOARD;
+
   const withMemberships = useSelector((state) => {
+    if (withContextTitle) {
+      return false;
+    }
+
     const boardMemberships = selectors.selectMembershipsForCurrentBoard(state);
 
     if (boardMemberships.length > 0) {
@@ -25,9 +37,19 @@ const BoardActions = React.memo(() => {
     return selectors.selectIsCurrentUserManagerForCurrentProject(state);
   });
 
+  const [t] = useTranslation();
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.actions}>
+        {withContextTitle && (
+          <div className={styles.action}>
+            <div className={styles.contextTitle}>
+              <Icon name={BoardContextIcons[boardContext]} className={styles.contextTitleIcon} />
+              {t(`common.${boardContext}`)}
+            </div>
+          </div>
+        )}
         {withMemberships && (
           <div className={styles.action}>
             <BoardMemberships />
