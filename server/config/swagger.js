4910600e6c176cdf2078e5a245dbf043375fc3e4
Maksim Eltyshev <meltyshev@gmail.com>
2025-09-08 17:41:43 +0200

    chore: Add Swagger config

---
+++ server/config/swagger.js
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) 2024 PLANKA Software GmbH
+ * Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
+ */
+
+const version = require('../version');
+
+module.exports = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      version,
+      title: 'PLANKA API',
+      description:
+        'API documentation for PLANKA - Real-time Collaborative Kanban Board Application',
+      license: {
+        name: 'Fair Use License',
+        url: 'https://github.com/plankanban/planka/blob/master/LICENSE.md',
+      },
+    },
+  },
+  apis: ['./api/controllers/**/*.js', './api/models/*.js', './api/responses/*.js'],
+};
