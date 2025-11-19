6e44b2ad195e13b0a40dc2a7d18559e426a3c93d
Maksim Eltyshev <meltyshev@gmail.com>
2025-11-19 18:20:54 +0100

    chore: Restore license header

--- server/utils/send_notifications.py
+++ server/utils/send_notifications.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2024 PLANKA Software GmbH
+# Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
+
 import sys
 import json
 import logging
