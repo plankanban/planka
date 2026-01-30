2421c7b4e514eeec361011f0f23453fb31abfd7c
i.elizentsev <i.elizentsev@gmail.com>
2026-01-30 17:43:27 +0700

    chore: enable server host configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    host: true,
   },
   build: {
     target: browserslistToEsbuild(['>0.2%', 'not dead', 'not op_mini all']),
