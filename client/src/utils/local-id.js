16b228e54e3ff6e2ec582242f7699da0ded10f59
Maksim Eltyshev <meltyshev@gmail.com>
2026-02-11 11:59:57 +0100

    fix: Prevent collisions when generating local ids

--- client/src/utils/local-id.js
+++ client/src/utils/local-id.js
@@ -3,6 +3,16 @@
  * Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
  */
 
-export const createLocalId = () => `local:${Date.now()}`;
+let lastTimestamp = 0;
+let sequence = 0;
+
+export const createLocalId = () => {
+  const timestamp = Date.now();
+
+  sequence = timestamp <= lastTimestamp ? sequence + 1 : 0;
+  lastTimestamp = timestamp;
+
+  return `local:${timestamp}-${String(sequence).padStart(4, '0')}`;
+};
 
 export const isLocalId = (id) => id.startsWith('local:');
